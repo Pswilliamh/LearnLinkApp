af77e5e70f355c2973093e72e65f7ee4545347c5
William Hardrick (Ambassador) <PsWilliamh@gmail.com>
2025-05-16 16:08:56 +0000

    Well it is one step closer praise God the placeholder images are there but they're not showing up as images that just says 600x400 in a white background

--- src/next.config.ts
+++ src/next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
         port: '',
-        pathname: '/**',
+        pathname: '/*x*.png', // More specific pattern for WIDTHxHEIGHT.png
       },
     ],
   },
